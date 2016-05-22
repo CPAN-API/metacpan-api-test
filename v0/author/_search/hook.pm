424a689d8d8a2d7bffbc783b0877dc6741425718
Alexey Melezhik <melezhik@gmail.com>
2016-05-22 22:31:41 +0300

    up!

--- v0/author/_search/hook.pm
+++ v0/author/_search/hook.pm
@@ -5,8 +5,8 @@ set_response_processor( sub {
       my $headers   = shift;
       my $body      = shift;
 
-      my $hits  =  join "\n", sort keys %{decode_json($body)->{hits}};
+      my $flds  =  join "\n", sort keys %{decode_json($body)};
 
-      return $headers."$hits";
+      return $headers."$flds";
 
 } );
